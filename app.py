183a91966a43982e65d4e9dba32b1ccef0ee288b
Redo08 <juandavidreyesortiz8@gmail.com>
2025-05-03 15:32:54 -0500

    Cambio nombre mapa vacio

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ ANCHO, ALTO = 1300, 650
 # Instancias
 helpers = Helpers()
 grafo = Grafo()
-data = helpers.cargar_texto_manual("archivos/pordefecto.json")
+data = helpers.cargar_texto_manual("archivos/MapaxVacio.json")
 grafo.cargar_json(data)
 
 visualizador = Visualizador(grafo, ANCHO, ALTO)
